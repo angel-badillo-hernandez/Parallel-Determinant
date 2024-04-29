d7b90e4899e70d1a99ac54143a3933d094de80a5
Angel Badillo Hernandez <badilloa022402@gmail.com>
2024-04-28 21:46:40 -0500

    added scripts

---
+++ BadilloCallender_A2Script_T16_1024.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Angel Badillo, Chad Callender
+# Parallel (16, 1024) Frontera script
+
+#SBATCH -J myparalleljob_16_1024           # Job name
+#SBATCH -o myparalleljob_16_1024.o%j       # Name of stdout output file
+#SBATCH -e myparalleljob_16_1024.e%j       # Name of stderr error file
+#SBATCH -p development     # Queue (partition) name
+#SBATCH -N 1               # Total # of nodes (1 for OpenMP or Threads)
+#SBATCH -n 1               # Total # of mpi tasks (1 for OpenMP or Threads)
+#SBATCH -t 00:2:00         # Run time (hh:mm:ss)
+#SBATCH --mail-type=all    # Send email at begin and end of job
+#SBATCH -A ASC23018       # Project/Allocation name (req'd if you have more than 1)
+#SBATCH --mail-user=username@tacc.utexas.edu
+
+# Any other commands must follow all #SBATCH directives...
+module list
+pwd
+date
+
+# Compile parallel code
+g++ BadilloCallendar_T16_1024.cpp -o parallel_16_1024
+
+# Set thread count
+export OMP_NUM_THREADS=16
+
+# Launch C++ multithreaded code
+./parallel_16_1024
+
+# # Launch parallel code...
+# # loop runs code three times
+# for value in {1..3}
+# do
+#     ibrun ./parallel_16_1024       # Use ibrun instead of mpirun or mpiexec
+#     echo ''              # newline
+# done
--- BadilloCallender_Script_Seq_1024.sh
+++ BadilloCallender_Script_Seq_1024.sh
@@ -6,7 +6,7 @@
 #SBATCH -J myserialjob_1024           # Job name
 #SBATCH -o myserialjob_1024.o%j       # Name of stdout output file
 #SBATCH -e myserialjob_1024.e%j       # Name of stderr error file
-#SBATCH -p small           # Queue (partition) name
+#SBATCH -p development           # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
 #SBATCH -t 00:2:00        # Run time (hh:mm:ss)
@@ -20,12 +20,15 @@ pwd
 date
 
 # Compile serial code
-gcc Badillo_Callender_SeqVer_1024.c -lm -o serial_1024
+g++ BadilloCallender_Seq_1024.cpp -o serial_1024
 
 # Launch serial code...
-# loop runs code three times
-for value in {1..3}
-do
-    ./serial_1024        # Do not use ibrun or any other MPI launcher
-    echo ''              # newline
-done
+./serial_1024
+
+# # Launch serial code...
+# # loop runs code three times
+# for value in {1..3}
+# do
+#     ./serial_1024        # Do not use ibrun or any other MPI launcher
+#     echo ''              # newline
+# done
